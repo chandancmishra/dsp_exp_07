74ea00b548eecd9db59959fa8b38dfc540c8f7f2
chandancmishra <chandan5108@gmail.com>
2023-07-17 11:37:52 +0530

    updated file

---
+++ a7.js
@@ -0,0 +1,17 @@
+var ht;
+var wd;
+
+function  showDiv(str)
+{
+	if(str == '1')
+	{
+		document.getElementById("part1Div").style.display = '';
+		document.getElementById("part2Div").style.display = 'none';
+	}
+	else
+	{
+		document.getElementById("part2Div").style.display = '';
+		document.getElementById("part1Div").style.display = 'none';
+	}
+	
+}
